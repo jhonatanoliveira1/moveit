cf022532457a86a8b0502ae2a758a549860557f4
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-04-24 00:17:48 -0300

    finalização dos contexts

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/jhonatanoliveira1.png" alt="Jhonatan de Oliveira"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Jhonatan de Oliveira</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import { ChallengesProvider } from '../contexts/ChallengesContext'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  
-
   return (
     <ChallengesProvider>
       <Component {...pageProps} />
